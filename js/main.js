7059813fb801848c309bd5f41b3f5366d1a1e7f7
mrLucianoii <luciano@brandedsolid.com>
2016-11-12 17:01:27 -0800

    Start Game Animation and board layout added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+"use strict";
+
 var toggleMenu = function(target){
-    $("#menu").toggleClass("open");
-    console.log(target);
+    $(".menu").toggleClass("open");
     
+}
+var start = function(target){
+        console.log(target);
+
+    $("board, block").toggleClass("start");
 }
 
 jQuery(document).ready(function ($) {
